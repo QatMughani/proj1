aba2ca7bdfb0215e36382052a6887139cecc1ab5
QatMughani <qatmughani@gmail.com>
2023-05-08 02:05:21 -0400

    First commit

---
+++ first_example.py
@@ -0,0 +1,18 @@
+p = 5
+r = 10
+L = list(range(0, 101))
+K = [x for x in L if x % 2 == 1 or x % 2 == 0]
+R = list(range(3,6))
+
+Y = [x for x in range(0, 101) if x % 4 == 1 or x % 3 == 0]
+W = [x for x in range (0,31) if x % 3  == 0]
+D = {'eggs':10, 'banana':5}
+y = 'hello'
+J = [0,1,2]
+T = [2**x for x in range(0,11)]
+W.reverse()
+if __name__ == '__main__':
+    print(W)
+
+
+
